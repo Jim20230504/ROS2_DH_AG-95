aa310e245cff43b633519590a265a5377477e3b1
jim@123.com <jim@123.com>
2025-12-24 14:37:05 +0800

    取消跟踪

--- build/dh_gripper_description/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/jim/dh_gripper_ws/install/dh_gripper_msgs/share/dh_gripper_msgs/package.sh"
-. "/home/jim/dh_gripper_ws/install/dh_gripper_driver/share/dh_gripper_driver/package.sh"
